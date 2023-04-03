6d272353b825c5d157d0d07e8be1daf5e4c5fb64
4strodev <4strodev@example.com>
2023-04-03 17:16:34 +0200

    commenting main.js

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const express = require("express");
 
+// Initializing express
 const app = express();
 
+
+// Starting server
 app.listen("3000", () => {
     console.log("Listening on port 3000");
 });
